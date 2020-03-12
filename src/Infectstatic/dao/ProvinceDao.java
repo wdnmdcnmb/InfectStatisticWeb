754b76572d204bc7d67691a32db5edb5712ad1aa
An1ess <An1ess@users.noreply.github.com>
2020-03-12 13:50:55 +0800

    okk

--- src/Infectstatic/dao/ProvinceDao.java
+++ src/Infectstatic/dao/ProvinceDao.java
@@ -6,8 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProvinceDao {
-    List<Province> list(){
+    public List<Province> list(){
         List<Province> provinces=new ArrayList<>();
+        for(int i=0;i<36;i++){//测试
+            Province a=new Province();
+            a.name="ss";
+            a.curePeople=100;
+            a.deadPeople=10;
+            a.doubtPeople=20;
+            a.infectPeople=30;
+            provinces.add(a);
+        }
         return provinces;
     }
 }
---
+++ src/Infectstatic/servelet/ProvinceServlet.java
@@ -0,0 +1,26 @@
+package Infectstatic.servelet;
+
+import Infectstatic.dao.ProvinceDao;
+import Infectstatic.pojo.Province;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ProvinceServlet")
+public class ProvinceServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProvinceDao a=new ProvinceDao();
+        List<Province> provinces= a.list();
+        request.setAttribute("provinces",provinces);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
